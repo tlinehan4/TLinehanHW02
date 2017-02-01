1a7818d37310c4bc375ce5291d1fbd79cffe9abe
Timothy Linehan <timlinehan@timothys-mbp-2.bc.edu>
2017-02-01 14:44:14 -0500

    Initial Commit

---
+++ $125 Million AppTests/_125_Million_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  _125_Million_AppTests.swift
+//  $125 Million AppTests
+//
+//  Created by Timothy Linehan on 2/1/17.
+//  Copyright © 2017 Timothy Linehan. All rights reserved.
+//
+
+import XCTest
+@testable import _125_Million_App
+
+class _125_Million_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
