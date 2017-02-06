a40d67af14495fa6d0fdb32be624e16065e10bab
Timothy Linehan <timlinehan@timothys-mbp-2.bc.edu>
2017-02-05 20:39:13 -0500

    Final

--- $125 Million App/ViewController.swift
+++ $125 Million App/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,23 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        
+        if let miles = Double(userInput.text!) {
+            let km = miles * 1.6
+            resultsLabel.text = "\(miles) miles = \(km) kilometers"
+        } else {
+            resultsLabel.text = ""
+            
+            let alertController = UIAlertController(title: "Entry Error", message: "Please enter a valid number. Not an empty string, no commas, symbols, or non-numeric characters", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        }
+        
+        
     }
 
 
